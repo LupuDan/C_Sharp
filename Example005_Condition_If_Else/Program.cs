9707bcfe66efa0b1a5d7bf3b53774013afce453e
BLMgreatAgain <dlupu666@gmail.com>
2022-08-10 21:36:07 +0300

    add condition

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insertati numele utilizatorului: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masa")
+{
+    Console.WriteLine("WOW, este MASHA!");
+}
+else
+{
+    Console.Write("Buna, ");
+    Console.WriteLine(username);
+}
